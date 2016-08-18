11ff7925538b3e019a578a35a3f228c78437c417
trandangthanh <trandangthanh@outlook.com>
2016-08-18 09:48:25 +0700

    fixed references

---
+++ Me.WebForm.Sample.v1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Me.WebForm.Sample.v1.Global" Language="C#" %>
---
+++ Me.WebForm.Sample.v1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Me.WebForm.Sample.v1.Startup))]
+namespace Me.WebForm.Sample.v1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
